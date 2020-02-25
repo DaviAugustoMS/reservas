23733bb78bc10d427e86d60aaa89f48aa3edea1a
Davi augusto <daviaugusto.developer@gmail.com>
2020-02-25 20:46:26 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/icon.svg';
+
+import './stylles.css';
+
+
+export default function Header() {
+ return (
+   <header className="container">
+     <Link to="/">
+      <img className='logo' src={Logo} alt="logo"/>
+     </Link>
+
+     <Link className="reservas" to="/reservas">
+      <div>
+        <strong>Minhas reservas</strong>
+        <span>3 reservas</span>
+      </div>
+     </Link>
+   </header>
+  );
+}
