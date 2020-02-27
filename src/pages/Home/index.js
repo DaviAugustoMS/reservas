a1090e1b22d3c349c7825f174268ff7f4c30087f
Davi augusto <daviaugusto.developer@gmail.com>
2020-02-26 21:19:09 -0300

    home, reservas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { MdFlightTakeoff } from 'react-icons/md';
+
+import api from '../../services/api';
 
 export default function Home() {
+
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+
+    async function loadApi(){
+      const response = await api.get('trips')
+      setTrips(response.data);
+
+    }
+
+    loadApi();
+  }, [])
  return (
-   <h1>Home</h1>
+   <div>
+     <div className="box">
+      {trips.map(trip => (
+        <li key={trip.id}>
+          <img src={trip.image} alt={trip.title}/>
+          <strong>{trip.title}</strong>
+          <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'} </span>
+          <button 
+            type="button"
+            onClick={() => {}}  
+          >
+            <div>
+              <MdFlightTakeoff size={16} color="#fff" />
+            </div>
+            <span>SOLICITAR A RESERVA</span>
+          </button>
+        </li>
+      ))}
+     </div>
+   </div>
   );
 }
